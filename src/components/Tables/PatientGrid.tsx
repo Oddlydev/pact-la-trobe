cebb34489abbd113e721ef70ce5b455d6583dc4d
SheharaF01 <shehara.oddly@gmail.com>
2025-09-29 15:45:08 +0530

    Update PatientGrid.tsx

--- src/components/Tables/PatientGrid.tsx
+++ src/components/Tables/PatientGrid.tsx
@@ -20,7 +20,7 @@ export default function PatientGrid({ patients }: Props) {
     <div className="pb-4">
       <div className="grid grid-cols-1 gap-x-4 gap-y-4 sm:grid-cols-2 lg:grid-cols-3">
         {patients.map((p, i) => (
-          <PatientCard key={i} {...p} />
+          <PatientCard id={""} key={i} {...p} />
         ))}
       </div>
     </div>
